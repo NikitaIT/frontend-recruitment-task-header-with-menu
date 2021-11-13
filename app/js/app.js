aca932263bc581c9cd769511b1bb4158a0251afa
NikitaIT <NIKITA2008-101@yandex.ru>
2021-11-13 20:13:15 +0200

    feat(ie11): add normalize if no custom properties

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,18 @@
+import { loadCss } from "./loadCss.mjs";
 document.addEventListener("DOMContentLoaded", () => {
+	loadIe11CSS();
 });
+
+function loadIe11CSS() {
+	var hasCustomPropertiesSupport =
+		(window.CSS && window.CSS.supports.bind(window.CSS)) || window.supportsCSS;
+	if (
+		!(
+			!!hasCustomPropertiesSupport &&
+			(hasCustomPropertiesSupport("--f:0") ||
+				hasCustomPropertiesSupport("--f", 0))
+		)
+	) {
+		loadCss("css/normalize.css");
+	}
+}
